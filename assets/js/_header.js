79cf5e6a9cd093da1242a7ca94f36affb967dca2
Iago Bozza <iagobozza@writeonlycode.io>
2023-01-04 10:31:35 +0530

    Update theme

--- assets/js/_header.js
+++ assets/js/_header.js
@@ -1,38 +1,38 @@
-document.addEventListener("DOMContentLoaded", () => {
-  if ( window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ) {
-    document.body.classList.add("dark");
-  } else if ( window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches ) {
-    document.body.classList.remove("dark");
-  }
+// document.addEventListener("DOMContentLoaded", () => {
+//   if ( window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ) {
+//     document.documentElement.setAttribute('data-bs-theme', 'dark')
+//   } else if ( window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches ) {
+//     document.documentElement.setAttribute('data-bs-theme', 'light')
+//   }
 
-  if ( localStorage.getItem("isDarkMode") === "true" ) {
-    document.body.classList.add("dark");
-    localStorage.setItem('isDarkMode', true);
-  } else if ( localStorage.getItem("isDarkMode") === "false" ) {
-    document.body.classList.remove("dark");
-    localStorage.setItem('isDarkMode', false);
-  }
+//   if ( localStorage.getItem("isDarkMode") === "true" ) {
+//     document.documentElement.setAttribute('data-bs-theme', 'dark')
+//     localStorage.setItem('isDarkMode', true);
+//   } else if ( localStorage.getItem("isDarkMode") === "false" ) {
+//     document.documentElement.setAttribute('data-bs-theme', 'light')
+//     localStorage.setItem('isDarkMode', false);
+//   }
 
-  const lightDarkToggler = document.getElementById("light-dark-toggler");
+//   const lightDarkToggler = document.getElementById("light-dark-toggler");
 
-  lightDarkToggler.addEventListener("click", (e) => {
-    e.preventDefault();
-    lightDarkToggle();
-  });
-});
+//   lightDarkToggler.addEventListener("click", (e) => {
+//     e.preventDefault();
+//     lightDarkToggle();
+//   });
+// });
 
-function lightDarkToggle() {
+// function lightDarkToggle() {
 
-  console.log(localStorage.getItem("isDarkMode"));
+//   console.log(localStorage.getItem("isDarkMode"));
 
-  if ( localStorage.getItem("isDarkMode") === "true" ) {
-    document.body.classList.remove("dark");
-    localStorage.setItem('isDarkMode', false);
-  } else if ( localStorage.getItem("isDarkMode") === "false" ) {
-    document.body.classList.add("dark");
-    localStorage.setItem('isDarkMode', true);
-  } else {
-    document.body.classList.remove("dark");
-    localStorage.setItem('isDarkMode', false);
-  }
-}
+//   if ( localStorage.getItem("isDarkMode") === "true" ) {
+//     document.documentElement.setAttribute('data-bs-theme', 'light')
+//     localStorage.setItem('isDarkMode', false);
+//   } else if ( localStorage.getItem("isDarkMode") === "false" ) {
+//     document.documentElement.setAttribute('data-bs-theme', 'dark')
+//     localStorage.setItem('isDarkMode', true);
+//   } else {
+//     document.documentElement.setAttribute('data-bs-theme', 'light')
+//     localStorage.setItem('isDarkMode', false);
+//   }
+// }
--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module gitlab.com/writeonlyhugo/writeonlyhugo-theme
 go 1.17
 
 require (
-	gitlab.com/writeonlyhugo/hugo-module-bootstrap v0.0.0-20220816084509-b8f26558d42c // indirect
+	gitlab.com/writeonlyhugo/hugo-module-bootstrap v0.0.0-20230104032709-256ccad7a64e // indirect
 	gitlab.com/writeonlyhugo/hugo-module-bootstrap-icons v0.0.0-20220504151031-e4d7cba5d59b // indirect
 )
